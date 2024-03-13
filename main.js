cfd5d8db24403a9b96842e5e89d0c209764bcdd8
BLEAX <rlawjdgns135@naver.com>
2024-03-13 20:16:34 +0900

    firebase 모듈 업데이트

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-var express = require("express");
-var app = express();
-var fs = require("fs"); // 파일시스템 모듈 (Node.js 기본모듈)
-var sanitizeHTML = require("sanitize-html"); // XSS 방지를 위한 보안용 모듈
-var path = require("path"); // 경로 정보 보안용 모듈 (Node.js 기본모듈)
-var qs = require("querystring"); //url 객체의 query를 해석, 포맷팅하는 모듈 (Node.js 기본모듈)
-var url = require("url"); // url 객체화용 모듈 (Node.js 기본모듈)
+//파이어베이스 모듈
+const firebase = require("firebase");
+const firebaseui = require("firebaseui");
+const firebaseapp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+//express, 기본 Node.js 모듈
+const express = require("express");
+const app = express();
+const fs = require("fs"); // 파일시스템 모듈 (Node.js 기본모듈)
+const sanitizeHTML = require("sanitize-html"); // XSS 방지를 위한 보안용 모듈
+const path = require("path"); // 경로 정보 보안용 모듈 (Node.js 기본모듈)
+const qs = require("querystring"); //url 객체의 query를 해석, 포맷팅하는 모듈 (Node.js 기본모듈)
+const url = require("url"); // url 객체화용 모듈 (Node.js 기본모듈)
 
 app.get("/", (req, res) => {
   return res.send("Project team 3 main page");
@@ -15,9 +23,3 @@ app.get("/page", (req, res) => {
 });
 
 app.listen(3000, () => {});
-
-/*
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  */
