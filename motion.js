4efbe526b5bd2f997dc0d8aae19b513a22fa834c
스윗남 (Kotlinsun) <lsk8597@naver.com>
2024-05-03 23:03:30 +0900

    메인 카테고리 수정

--- motion.js
+++ motion.js
@@ -1,2 +1,10 @@
 
   
+			function toggleMenu() {
+        var menu = document.querySelector('.menu');
+        if (menu.style.left === '-300px') {
+          menu.style.left = '0';
+        } else {
+          menu.style.left = '-300px';
+        }
+      }
