a5f8da0b87bde6b6d2e876affa4489721a3906f3
스윗남 (Kotlinsun) <lsk8597@naver.com>
2024-05-11 17:54:13 +0900

    로그인 문제 해결

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc, collection } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBhhfQkeuxCSjcZ-71QLyUf84ic9AiwDT4",
@@ -13,11 +12,43 @@ const firebaseConfig = {
     appId: "1:344314833740:web:e0499ad9aba9ab880e3de9"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth();
-const db = getFirestore();
 
+
+// Firebase 앱 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firebase 인증(Authentication) 서비스 초기화
+const auth = getAuth(app);
+
+// Firebase Firestore 서비스 초기화
+const db = getFirestore(app);
+
+// 사용자 로그인 후에 실행할 함수 정의
+function handleLoginSuccess(user) {
+    // 사용자의 데이터를 가져옴
+    const userRef = doc(collection(db, 'users'), user.uid); // 수정된 부분
+    
+    getDoc(userRef).then((doc) => {
+        if (doc.exists()) {
+            const userData = doc.data();
+            
+            // 필드가 null인지 확인하고 페이지 이동
+            if (userData.personal_color === null || userData.recommend_product === null || userData.skin_concern === null || userData.skin_type === null) {
+                console.log('Some user data is missing. Redirecting to select1 page.');
+                location.href = '/select1';
+            } else {
+                console.log('All user data is available. Redirecting to main page.');
+                location.href = '/main';
+            }
+        } else {
+            console.log('User document does not exist.');
+        }
+    }).catch((error) => {
+        console.log('Error getting user document:', error);
+    });
+}
+
+// 로그인 버튼 클릭 시 호출되는 함수
 window.login = function (e) {
     e.preventDefault();
 
@@ -28,11 +59,14 @@ window.login = function (e) {
         .then((userCredential) => {
             const user = userCredential.user;
             alert('로그인에 성공했습니다.');
-            location.href = '/select1'
+            
+            // 로그인 후에 사용자의 데이터 확인 및 처리 함수 호출
+            handleLoginSuccess(user);
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.log(errorMessage);
             alert("다시 입력해주세요.");
             document.querySelectorAll("input[class=form-control]")[0].value = null;
             document.querySelectorAll("input[class=form-control]")[1].value = null;
